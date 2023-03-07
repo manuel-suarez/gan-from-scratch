71e1eea867311ec71fe32abfe1dac8f9e669cc61
Arturo Suárez <manuel.suarez@cimat.mx>
2023-03-07 11:15:13 -0600

    Discriminator

--- main.py
+++ main.py
@@ -34,3 +34,11 @@ class Generator(nn.Module):
     def forward(self, x):
         return self.activation(self.dense_layer(x))
 
+class Discriminator(nn.Module):
+    def __init__(self, input_length: int):
+        super(Discriminator, self).__init__()
+        self.dense = nn.Linear(input_length, 1)
+        self.activation = nn.Sigmoid()
+
+    def forward(self, x):
+        return self.activation(self.dense(x))
