d331486fef6ca0d9906e0535d9e9b70eeb8ee32b
Arturo Suárez <manuel.suarez@cimat.mx>
2023-03-07 11:01:05 -0600

    Initial version, data generation

---
+++ main.py
@@ -0,0 +1,25 @@
+import math
+import numpy as np
+from typing import List, Tuple
+
+def create_binary_list_from_int(number: int) -> List[int]:
+    if number < 0 or type(number) is not int:
+        raise ValueError("Only Positive integers are allowed")
+
+    return [int(x) for x in list(bin(number))[2:]]
+
+def generate_even_data(max_int: int, batch_size: int=16) -> Tuple[List[int], List[List[int]]]:
+    # Get the number of binary places needed to represent the maximum number
+    max_length = int(math.log(max_int, 2))
+
+    # Sample batch_size number of integers in range 0-max_int
+    sampled_integers = np.random.randint(0, int(max_int / 2), batch_size)
+
+    # create a list of labels all ones because all numbers are even
+    labels = [1] * batch_size
+
+    # Generate a list of binary numbers for training.
+    data = [create_binary_list_from_int(int(x * 2)) for x in sampled_integers]
+    data = [([0] * (max_length - len(x))) + x for x in data]
+
+    return labels, data
